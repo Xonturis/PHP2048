97edcad36f19bf0fbd6f80e4f9ba68087196f336
Lyxas <lucas.gazeau@etu.univ-nantes.fr>
2020-11-14 18:00:27 +0100

    fix syntax

--- ressources/vue/MainPageVue.php
+++ ressources/vue/MainPageVue.php
@@ -1,21 +1,20 @@
 <?php
-require_once PATH_VUE."/plateau/PlateauVue.php";
-require_once PATH_VUE."/classement/ClassementVue.php";
+require_once PATH_VUE . "/plateau/PlateauVue.php";
+require_once PATH_VUE . "/classement/ClassementVue.php";
 
-class MainPageVue{
-
-    public static function getHtml($toDisplay=NULL){
+class MainPageVue
+{
+    public static function getHtml($toDisplay = NULL)
+    {
         ?>
         <div class="">
             <?php
 
             ?>
         </div>
-<<<<<<< Updated upstream
-<?php
-=======
-        <?php
->>>>>>> Stashed changes
-    }
 
+        <?php
+
+
+    }
 }

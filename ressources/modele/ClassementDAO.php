52e74746bce1def024fa99f3f319d89f5004d49c
Lucas Gazeau <lucas.gazeau@etu.univ-nantes.fr>
2020-12-13 15:14:19 +0100

    le commit de la mort

--- ressources/modele/ClassementDAO.php
+++ ressources/modele/ClassementDAO.php
@@ -3,7 +3,7 @@
 class ClassementDao{
 
     public static function getElements(int $nb) : ?Classement{
-        $statement = SqliteConnexion::getInstance()->getConnexion()->prepare('SELECT pseudo,score FROM PARTIES where gagne=1 ORDER BY score ASC LIMIT :nb;');;
+        $statement = SqliteConnexion::getInstance()->getConnexion()->prepare('SELECT pseudo,score FROM PARTIES where gagne=1 ORDER BY score ASC LIMIT :nb;');
         $statement->bindParam(':nb', $nb);
         $statement->execute();
         $result = $statement->fetchAll();
@@ -16,8 +16,12 @@ class ClassementDao{
     }
 
     public static function addElement($score){
-        $statement = SqliteConnexion::getInstance()->getConnexion()->prepare('INSERT INTO PARTIE VALUES();');;
-        $statement->bindParam(':nb', $nb);
+        $statement = SqliteConnexion::getInstance()->getConnexion()->prepare('INSERT INTO PARTIE VALUES(:pseudo,:gagne,:score);');
+        $statement->bindParam(':pseudo', $score->getName());
+        $statement->bindParam(':gagne', $score->getGagne());
+        $statement->bindParam(':score', $score->getScore());
+
+
         $statement->execute();
     }
 }

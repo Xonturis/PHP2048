4211ff08910f2c26959156449a2dccfcd337ca68
Lylian Siffre <lylian.s@hotmail.Fr>
2020-12-15 11:28:27 +0100

    fix endgame

--- ressources/controleur/FinPartieControleur.php
+++ ressources/controleur/FinPartieControleur.php
@@ -5,15 +5,23 @@ class FinPartieControleur
 {
 
     public static function perdu() {
-        HeaderVueConnected::getHtml($_SESSION["user"]->getPseudo());
+        $user = $_SESSION["user"];
+        HeaderVueConnected::getHtml($user->getPseudo());
         DefaiteVue::getHtml($_GET["data"]);
         FooterVue::getHtml();
+        $plateau = PlateauDAO::getOrCreateCurrentPlateau($user);
+        $plateau->reset();
+        PlateauDAO::savePlateauToDB($plateau, $user);
     }
 
     public static function gagne() {
-        HeaderVueConnected::getHtml($_SESSION["user"]->getPseudo());
+        $user = $_SESSION["user"];
+        HeaderVueConnected::getHtml($user->getPseudo());
         VictoireVue::getHtml($_GET["data"]);
         FooterVue::getHtml();
+        $plateau = PlateauDAO::getOrCreateCurrentPlateau($user);
+        $plateau->reset();
+        PlateauDAO::savePlateauToDB($plateau, $user);
     }
 
 }

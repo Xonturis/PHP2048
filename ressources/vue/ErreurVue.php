46556efa82287e13c2d3eb03aaecee294ad4813c
Lyxas <lucas.gazeau@etu.univ-nantes.fr>
2020-12-17 20:23:40 +0100

    ajout erreur

--- ressources/vue/ErreurVue.php
+++ ressources/vue/ErreurVue.php
@@ -1,14 +1,34 @@
 <?php
 
-require_once PATH_VUE."/IVue.php";
+require_once PATH_VUE . "/IVue.php";
 
 class ErreurVue implements IVue
 {
 
     public static function getHtml($toDisplay = NULL)
     {
+        if (isset($_GET["user"])) {
+            HeaderVueConnected::getHtml();
+        } else {
+            HeaderVue::getHtml();
+        }
+
+
         ?>
-        Ah, une erreur ! :o
+        <div class="erreur">
+            <?php if ($toDisplay == NULL) { ?>
+                <div class="text-center text-danger border border-danger bg-white">
+                    <p>Une erreur s'est produite ! Si ça marche pas chez nous, vous pouvez allez voir ailleurs : <a href="https://2048.loicyeu.fr">https://2048.loicyeu.fr</a></p>
+                </div>
+            <?php }
+            else { ?>
+                <div class="text-center text-danger border border-danger">
+                    <p>Une erreur s'est produite ! <?php $toDisplay ?> </p>
+                </div>
+            <?php } ?>
+        </div>
         <?php
+
+        FooterVue::getHtml();
     }
 }
--- ressources/vue/utilisateurs/InscriptionVue.php
+++ ressources/vue/utilisateurs/InscriptionVue.php
@@ -12,11 +12,6 @@ class InscriptionVue implements IVue
         ?>
         <form method="GET" class="connexionForm">
             <div class="container">
-                <div class="text-center text-danger border border-danger">
-                    <?php  if(isset($_GET["error"]) && $_GET["error"]){
-                        echo "L'utilisateur existe déjà !";
-                    }?>
-                </div>
                 <h3 id="title">S'inscrire</h3>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Pseudo</label>
